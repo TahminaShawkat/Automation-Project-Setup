9cc2f341972e07647c66e29c8676238df088cbd5
Tahmina Shawkat <t.shawkat.t@gmail.com>
2022-01-29 06:26:31 -0500

    Updated the code with method scenerio

--- AutomationAssignment1/src/main/java/com/AutomationAssignment1/AutomationAssignment1.java
+++ AutomationAssignment1/src/main/java/com/AutomationAssignment1/AutomationAssignment1.java
@@ -1,37 +1,76 @@
 package com.AutomationAssignment1;
 
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.edge.EdgeDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.opera.OperaDriver;
+import org.openqa.selenium.edge.EdgeDriver;
 
-public class AutomationAssignment1 {
-    public static void main(String[] args) throws  Exception {
-        //launch browser Firefox
-        System.setProperty("webdriver.gecko.driver","./src/main/resources/geckodriver.exe");
-        WebDriver firfox_driver = new FirefoxDriver();   //FirefoxDriver=interface
-        firfox_driver.get("https://www.google.com/");
-        firfox_driver.close();
+public class AutomationAssignment1{
+    public static WebDriver driver;
 
-        //launch browser Chrome
-        System.setProperty("webdriver.chrome.driver","./src/main/resources/chromedriver.exe");
-        WebDriver driver = new ChromeDriver();
-        driver.get("https://www.google.com/");
-        driver.close();
-
-        //launch browser Opera
-        System.setProperty("webdriver.chrome.driver", "./src/main/resources/operadriver.exe");
-        WebDriver opera_driver = new ChromeDriver();
-        opera_driver.get("https://www.google.com/");
-        opera_driver.close();
-
-        //launch browser Edge
-        System.setProperty("webdriver.edge.driver", "./src/main/resources/msedgedriver.exe");
-        WebDriver edge_driver = new EdgeDriver();
-        edge_driver.get("https://www.google.com/");
-        edge_driver.close();
-
-
-
+    public static void main(String[] args) throws InterruptedException {
+        Chrome_Launch();
+        Chrome_Wait();
+        Chrome_Close();
+        Firefox_Launch();
+        Firefox_Wait();
+        Firefox_Close();
+        Opera_Launch();
+        Opera_Wait();
+        Opera_Close();
+        Edge_Launch();
+        Edge_Wait();
+        Edge_Close();
     }
+    // Chrome Launch, Open website, close
+    public static void Chrome_Launch(){
+        System.setProperty("webdriver.chrome.driver","src/main/resources/chromedriver.exe");
+        driver = new ChromeDriver();
+        driver.get("https://www.google.com/");
+    }
+    public static void Chrome_Wait() throws InterruptedException {
+       Thread.sleep(1000);
+    }
+    public static void Chrome_Close(){
+        driver.close();
+    }
+    // Firefox Launch, Open website, close
+    public static void Firefox_Launch(){
+        System.setProperty("webdriver.gecko.driver","src/main/resources/geckodriver.exe");
+        driver = new FirefoxDriver();
+        driver.get("https://www.firefox.com/");
+    }
+    public static void Firefox_Wait() throws InterruptedException {
+        Thread.sleep(1000);
+    }
+    public static void Firefox_Close(){
+        driver.close();
+    }
+    // Opera Launch, Open website, close
+    public static void Opera_Launch(){
+        System.setProperty("webdriver.opera.driver","src/main/resources/operadriver.exe");
+        driver = new OperaDriver();
+        driver.get("https://www.opera.com/");
+    }
+    public static void Opera_Wait() throws InterruptedException {
+        Thread.sleep(1000);
+    }
+    public static void Opera_Close(){
+        driver.close();
+    }
+    // Edge Launch, Open website, close
+    public static void Edge_Launch(){
+        System.setProperty("webdriver.edge.driver","src/main/resources/msedgedriver.exe");
+        driver = new EdgeDriver();
+        driver.get("https://www.microsoft.com/en-us/edge");
+    }
+    public static void Edge_Wait() throws InterruptedException {
+        Thread.sleep(1000);
+    }
+    public static void Edge_Close(){
+        driver.close();
+    }
+
 }
+
